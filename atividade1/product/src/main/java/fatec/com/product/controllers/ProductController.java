8b0117d4729b9d26d451390d3f6d0248619f0e23
Gabriel <gabrieloliveira280807@gmail.com>
2026-03-30 15:16:49 -0300

    feat: corrigindo páginas e adicionando atividades feitas

---
+++ atividade1/product/src/main/java/fatec/com/product/controllers/ProductController.java
@@ -0,0 +1,59 @@
+package fatec.com.product.controllers;
+
+import java.util.ArrayList;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import fatec.com.product.models.Product;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+        private ArrayList<Product> products = new ArrayList();
+
+        public ProductController() {
+             
+            Product p1 = new Product(1L,
+                                     "Notebook",
+                                     3500.00,
+                                     "Notebook i7 512 16gb");
+            
+            Product p2 = new Product();
+            
+            p2.setId(2L);
+            p2.setName("Smartphone");
+            p2.setPrice(2000.00);
+            p2.setDescription("Smartphone android 128gb");
+            
+            Product p3 = new Product();
+            p3.setId(3L);
+            p3.setName("Tablet");
+            p3.setPrice(1500.00);
+            p3.setDescription("Tablet android 64gb");
+          
+            products.add(p1);
+            products.add(p2);   
+            products.add(p3);
+        }
+
+        @GetMapping("/{id}")
+        public Product getProductById(@PathVariable long id){
+            return products
+                    .stream()
+                    .filter(p -> p.getId() == id)
+                    .findFirst()
+                    .orElse(null);
+            
+        }
+
+
+        @GetMapping
+        public ArrayList<Product> getProducts() {
+            return products;
+        }
+
+}
