38cd1c787b0f47353b67fdf1eab09a5ac30c41b4
Gabriel <gabrieloliveira280807@gmail.com>
2026-05-25 00:11:54 -0300

    feat: Removendo validações que são desncessárias para a prova. Vou fazer em formato mais simples, apenas representando o diagrama pedido e endpoints. Apesar do código antigo estar de acordo com boas práticas quando trabalhamos com API, vou manter o formato mais simplitas para se preocupar apenas com logica

--- simulado2/preprova/src/main/java/simulado/com/preprova/controller/AlunoController.java
+++ simulado2/preprova/src/main/java/simulado/com/preprova/controller/AlunoController.java
@@ -22,37 +22,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class AlunoController {
     
     private ArrayList<Aluno> alunos = new ArrayList<>();
-    private ArrayList<Turma> turmas;
 
-
-    public AlunoController(ArrayList<Turma> turmas) {
-        this.turmas = turmas;
-    }
-   
     @GetMapping
     public ArrayList<Aluno> getAlunos() {
         return alunos;
     }
 
     @PostMapping
-    public ResponseEntity<String> createAluno(@RequestBody Aluno aluno ) {
-        if (aluno.getTurma() == null || aluno.getTurma().getCodigo() == null ) {
-            return ResponseEntity.badRequest().body("Aluno deve pertencer a uma turma.");
-        }
-
-        String codigoTurma = aluno.getTurma().getCodigo();
-        Turma turma = turmas.stream()
-                .filter(t -> t.getCodigo().equals(codigoTurma))
-                .findFirst()
-                .orElse(null);
-
-        if (turma == null) {
-            return ResponseEntity.badRequest().body("turma com código " + codigoTurma + " não encontrada.");
-        }
-
-        turma.adicionarAluno(aluno);
+    public String createAluno(@RequestBody Aluno aluno ) {
         alunos.add(aluno);
-        return ResponseEntity.ok("Aluno adiconando com sucesso");
+        return "Aluno adiconando com sucesso";
     }
 
     @DeleteMapping("/{id}")
