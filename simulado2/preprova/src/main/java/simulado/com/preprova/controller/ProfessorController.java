abc3f0dc7e7e18dd57c0b628be13b34c7e707e96
Gabriel <gabrieloliveira280807@gmail.com>
2026-05-25 00:29:44 -0300

    feat: adicionando ProfessorController e endpoint de criação de professor

---
+++ simulado2/preprova/src/main/java/simulado/com/preprova/controller/ProfessorController.java
@@ -0,0 +1,23 @@
+package simulado.com.preprova.controller;
+
+import java.util.ArrayList;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import simulado.com.preprova.model.Professor;
+
+@RestController
+@RequestMapping("professores")
+public class ProfessorController {
+    
+    private ArrayList<Professor> professores = new ArrayList<>();
+
+    @PostMapping
+    public String createProfessor(@RequestBody Professor professor) {
+        professores.add(professor);
+        return "professor adicionado com sucesso!!";
+    }
+}
