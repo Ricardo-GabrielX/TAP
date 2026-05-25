b42059ee0573560c11843fefc9d840b060bfd893
Gabriel <gabrieloliveira280807@gmail.com>
2026-05-25 00:16:18 -0300

    chore: limpa imports não utilizados

--- simulado2/preprova/src/main/java/simulado/com/preprova/controller/AlunoController.java
+++ simulado2/preprova/src/main/java/simulado/com/preprova/controller/AlunoController.java
@@ -4,17 +4,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import simulado.com.preprova.model.Aluno;
-import simulado.com.preprova.model.Turma;
 
 import java.util.ArrayList;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
